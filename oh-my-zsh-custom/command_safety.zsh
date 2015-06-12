8498df2e1930c495f299e131132d5f08e3cb8239
Rob Freiburger <rob@robfreiburger.com>
2015-06-12 02:17:47 -0400

    Adds starting customizations.

---
+++ oh-my-zsh-custom/command_safety.zsh
@@ -0,0 +1,4 @@
+# Prevent accidents...
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
---
+++ oh-my-zsh-custom/thefuck.zsh
@@ -0,0 +1,3 @@
+# auto-fixing command typos with thefuck
+# https://github.com/nvbn/thefuck
+alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
